bbe2fedfa268856dd532ff4dbfd60edffeb7f23c
Fernando Aponte <faponte1230@yahoo.com>
2023-08-12 17:31:51 -0400

    create header file

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header className="app-header">
+            <h1> Gym RATe </h1>
+            <h4> Rated gyms at your fingertips!</h4>
+        </header>
+    )
+}
+
+export default Header
