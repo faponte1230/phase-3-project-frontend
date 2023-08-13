8054fb0d51c171098f23f8a34358ef8e3d90be8c
Fernando Aponte <faponte1230@yahoo.com>
2023-08-13 05:05:34 -0400

    add CRUD functions and finalize

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Routes} from 'react-router-dom';
 import Header from './Header';
 import GymList from './GymList';
 import './App.css';
-
+import GymForm from './GymForm';
+import UpdateGymForm from './UpdateGymForm';
+import GymReviews from './GymReviews';
 
 
 function App() {
@@ -32,13 +34,35 @@ function App() {
     console.log('deleted')
   }
   
+  function addNewReview(newReview){
+    const currentGym = gyms.find((gym) => gym.id === newReview.gym_id)
+    currentGym.reviews.push(newReview)
+    const updatedgyms = gyms.map((gym) => gym.id === currentGym.id ? currentGym : gym)
+    setGyms(updatedgyms)
+  }
+
+  function deleteReview(deletedRev){
+    const currentGym = gyms.find((gym) => gym.id === deletedRev.gym_id)
+    const updatedReviews = currentGym.reviews.filter((review) => review.id !== deletedRev.id)
+    currentGym.reviews = updatedReviews
+    const updatedGyms = gyms.map((gym) => gym.id === currentGym.id ? currentGym : gym)
+    setGyms(updatedGyms)
+  }
+
   return (
     <div className="App">
       <Header />
       <br></br>
       <Routes>
-        <Route path='/' element={<GymList addGym={addGym} gyms={gyms} updateGym={updateGym} deleteGym={deleteGym}/>}/>
-        
+        <Route path='/' element={
+          <>
+            <GymForm addGym={addGym} />
+            <UpdateGymForm updateGym={updateGym} />
+            <GymList gyms={gyms} updateGym={updateGym} deleteGym={deleteGym}/>
+          </>}>
+        </Route>
+
+        <Route path='/reviews/:gym_id' element={<GymReviews gyms={gyms} addNewReview={addNewReview} deleteReview={deleteReview}/>} />
       </Routes>
     </div>
   );
