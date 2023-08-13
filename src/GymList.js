f917b2f0ae9db26dfb9a6c3e4201b56f8954e6c1
Fernando Aponte <faponte1230@yahoo.com>
2023-08-12 23:21:08 -0400

    import gym

--- src/GymList.js
+++ src/GymList.js
@@ -1,16 +1,16 @@
 import React from "react";
-//import Gym from './Gym';
+import Gym from './Gym';
 //import GymForm from "./GymForm";
 //import UpdateGym from "./UpdateGym";
 
 function GymList({ addGym, gyms , updateGym , deleteGym}){
-   /* 
+    
     let displayGyms = gyms.map( g => {
         return(
            <Gym key={g.id} gym={g} deleteGym={deleteGym} />
         )
     })
-*/
+
     return(
         <div className='home'>
             <div className='nav'>
